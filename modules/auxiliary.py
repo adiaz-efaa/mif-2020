56974fc0f45a2852643cfd9e31a792ddc6901cea
Alvaro Diaz <alvaro@efaa.cl>
2020-10-13 14:47:05 +0000

    Se agrega notebook 5, construcción de curva OIS.

--- modules/auxiliary.py
+++ modules/auxiliary.py
@@ -16,6 +16,15 @@ def show_leg(leg, leg_type:str, leg_subtype: str = '') -> pd.DataFrame:
     tabla = []
     for i in range(0, leg.size()):
         tabla.append(Qcf.show(leg.get_cashflow_at(i)))
+        
+    if leg_type == 'FixedRateMultiCurrencyCashflow':
+        columnas = ['fecha_inicial', 'fecha_final', 'fecha_pago', 'nominal', 'amortizacion',
+                    'interes', 'amort_es_cashflow', 'flujo_moneda_nocional',
+                    'moneda_nocional', 'valor_tasa', 'tipo_tasa', 'fecha_fixing_fx',
+                    'moneda_pago', 'codigo_indice_fx', 'valor_indice_fx',
+                    'amort_moneda_pago', 'interes_moneda_pago']
+        df = pd.DataFrame(tabla, columns=columnas)
+        return df
 
     columnas = list(Qcf.get_column_names(leg_type, leg_subtype))
     df = pd.DataFrame(tabla, columns=columnas)
